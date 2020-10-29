2bfcae1846a009b821920cc9adaf8a075f441614
xiangzhenwen <xiangzhenwen@yaochufa.com>
2020-10-29 09:34:45 +0800

    modify

--- StrSearch.java
+++ StrSearch.java
@@ -1,18 +1,30 @@
-import javafx.scene.chart.Chart;
 
 public class StrSearch {
     /**
-//         给定1个二维字符数组m和单词1个w，搜索w是否在m中。搜索的定义是从m的任意位置开始，可以上下左右移动，依次和w每个字符匹配，如果w能匹配完，则存在，否则不存在。
-//             - 例子："zoo"，"zoro"，"xtifx"都能够搜索成功，但"oto"搜索不成功。
-//             a c d z
-//             x t r o
-//             f i o o
+//Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,         
+Given board =
+
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+
+
+ https://www.cnblogs.com/xiaolovewei/p/8228100.html
      */
     public static void main(String[] args) {
         char[][] arr = new char[][] { { 'a', 'c', 'd', 'z' }, { 'x', 't', 'r', 'o' }, { 'f', 'i', 'o', 'o' } };
         boolean r = searchStr(arr, "ax3");
         System.out.println(r);
-    }
+    } 
 
     public static boolean searchStr(char[][] arr, String word) {
         int row = arr.length;
